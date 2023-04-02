0b38aeb2532fbe929b076336f03e9d4bab0c3212
ptnoire <ptnoire@gmail.com>
2023-04-02 00:11:43 -0400

    Gradient Text, Transparent Text, Video Backgrounds, Mobile first dynamics

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const ls1 = document.querySelector('.leftSideh1');
 const rs1 = document.querySelector('.rightSideh1');
+const targets = document.querySelectorAll('.target');
 
 ls1.dataset.text = ls1.textContent;
 rs1.dataset.text = rs1.textContent;
+
+const obsCallBack = function(entries, obs) {
+    const [entry] = entries;
+    console.log(entry);
+    const target = entry.target;
+    if(!entry.isIntersecting) return
+    target.classList.remove('hidden');
+    obs.unobserve(target);
+}
+
+const options = {
+    root: null,
+    threshold: 1,
+};
+  
+
+const observer = new IntersectionObserver(obsCallBack, options);
+  
+
+targets.forEach(el => {
+    el.classList.add('hidden');
+    observer.observe(el)
+});
+  
